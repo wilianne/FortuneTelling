cfafee82e2a10f5ce2b3e9cdec112fc793b3362b
Wilianne Paixao <williannequaresmapaixao@gmail.com>
2025-04-01 23:42:51 -0300

    end 2

--- game/fortunetelling.c
+++ game/fortunetelling.c
@@ -23,8 +23,7 @@ int main()
     printf("                          /          /   /                      \\\n");
     printf("\n\n");
     int second = time(0);
-    srand(second);//seed for it to leave random all the time
-
+    srand(second); // seed for it to leave random all the time
 
     int bignumber = rand();
     int numbersecret = bignumber % 100;
@@ -34,24 +33,24 @@ int main()
     int win;
 
     int level;
-    printf("Qual o nivel de dificuldade?\n");
-    printf("(1)Facil (2)Medio (3)Dificil\n\n");
+    printf("What is the difficulty level?\n");
+    printf("(1)Easy (2)Medium (3)Difficult\n\n");
     printf("Escolha: ");
-    scanf("%d",&level);
+    scanf("%d", &level);
 
     int numbertry;
 
-    switch(level){
-        case 1:
-            numbertry = 20;
-            break;
-        case 2:
-            numbertry = 15;
-            break;
-        default:
-            numbertry = 6;
-            break;
-
+    switch (level)
+    {
+    case 1:
+        numbertry = 20;
+        break;
+    case 2:
+        numbertry = 15;
+        break;
+    default:
+        numbertry = 6;
+        break;
     }
     /*if(level == 1){
         numbertry = 20;
@@ -63,7 +62,7 @@ int main()
         numbertry = 6;
     }*/
 
-    for(int i = 1;i <= numbertry;i++)
+    for (int i = 1; i <= numbertry; i++)
     {
         printf("What's your guess: ");
         printf("Try %d\n", i);
@@ -78,10 +77,11 @@ int main()
             continue; // return to the beginning of the loop i++
         }
 
-        win = (choise == numbersecret);   // 0 = F , 1 = T
+        win = (choise == numbersecret);       // 0 = F , 1 = T
         int higher = (choise > numbersecret); // 0 = F , 1 = T
 
-        if (win){
+        if (win)
+        {
             break;
         }
         else if (higher)
@@ -93,70 +93,72 @@ int main()
             printf("Your shot was lower than the number\n");
         }
 
-        double pointlost = abs(choise - numbersecret)/(double)2;
+        double pointlost = abs(choise - numbersecret) / (double)2;
 
         points = points - pointlost;
     }
     printf("END OF GAME!!\n");
-    if (win){
-            printf("Congratulations! You won\n");
-            printf("Your win in tentative %d\n", numbertry);
-            printf("Total points: %.1f\n",points);
+    if (win)
+    {
+        printf("Congratulations! You won\n");
+        printf("Your win in tentative %d\n", numbertry);
+        printf("Total points: %.1f\n", points);
 
-
-printf("                              __ooooooooo__\n");
-printf("                         oOOOOOOOOOOOOOOOOOOOOOo\n");
-printf("                       oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
-printf("                    oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
-printf("                  oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
-printf("                oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
-printf("               oOOOOOOOOOOO*  *OOOOOOOOOOOOOO*  *OOOOOOOOOOOOo\n");
-printf("              oOOOOOOOOOOO      OOOOOOOOOOOO      OOOOOOOOOOOOo\n");
-printf("              oOOOOOOOOOOOOo  oOOOOOOOOOOOOOOo  oOOOOOOOOOOOOOo\n");
-printf("              oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
-printf("              oOOOO     OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO     OOOOo\n");
-printf("              oOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOo\n");
-printf("               *OOOOO  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  OOOOO*\n");
-printf("                *OOOOOO  *OOOOOOOOOOOOOOOOOOOOOOOOOOOOO*  OOOOOO*\n");
-printf("                 *OOOOOO  *OOOOOOOOOOOOOOOOOOOOOOOOOOO*  OOOOOO*\n");
-printf("                  *OOOOOOo  *OOOOOOOOOOOOOOOOOOOOOOO*  oOOOOOO*\n");
-printf("                   *OOOOOOOo  *OOOOOOOOOOOOOOOOO*  oOOOOOOO*\n");
-printf("                     *OOOOOOOOo  *OOOOOOOOOOO*  oOOOOOOOO*\n");
-printf("                        *OOOOOOOOo           oOOOOOOOO*\n");
-printf("                           *OOOOOOOOOOOOOOOOOOOOO*\n");
-printf("                                ""ooooooooo"" \n");
-        }
-    else{
+        printf("                              __ooooooooo__\n");
+        printf("                         oOOOOOOOOOOOOOOOOOOOOOo\n");
+        printf("                       oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
+        printf("                    oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
+        printf("                  oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
+        printf("                oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
+        printf("               oOOOOOOOOOOO*  *OOOOOOOOOOOOOO*  *OOOOOOOOOOOOo\n");
+        printf("              oOOOOOOOOOOO      OOOOOOOOOOOO      OOOOOOOOOOOOo\n");
+        printf("              oOOOOOOOOOOOOo  oOOOOOOOOOOOOOOo  oOOOOOOOOOOOOOo\n");
+        printf("              oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo\n");
+        printf("              oOOOO     OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO     OOOOo\n");
+        printf("              oOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOo\n");
+        printf("               *OOOOO  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  OOOOO*\n");
+        printf("                *OOOOOO  *OOOOOOOOOOOOOOOOOOOOOOOOOOOOO*  OOOOOO*\n");
+        printf("                 *OOOOOO  *OOOOOOOOOOOOOOOOOOOOOOOOOOO*  OOOOOO*\n");
+        printf("                  *OOOOOOo  *OOOOOOOOOOOOOOOOOOOOOOO*  oOOOOOO*\n");
+        printf("                   *OOOOOOOo  *OOOOOOOOOOOOOOOOO*  oOOOOOOO*\n");
+        printf("                     *OOOOOOOOo  *OOOOOOOOOOO*  oOOOOOOOO*\n");
+        printf("                        *OOOOOOOOo           oOOOOOOOO*\n");
+        printf("                           *OOOOOOOOOOOOOOOOOOOOO*\n");
+        printf("                                "
+               "ooooooooo"
+               " \n");
+    }
+    else
+    {
         printf("You lost!Try again!\n");
 
-printf("                        oooo$$$$$$$$$$$$oooo\n");
-printf("                     oo$$$$$$$$$$$$$$$$$$$$$$$$o\n");
-printf("                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$\n");
-printf("   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$\n");
-printf("oo $ $ \"$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$\n");
-printf(" $$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$\n");
-printf("  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$\n");
-printf("  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  \"\"\"$$$\n");
-printf("    $$$\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$\n");
-printf("    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$o\n");
-printf("   o$$\"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o\n");
-printf("    $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\" \"$$$$$$ooooo$$$$o\n");
-printf("    o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$\n");
-printf("    $$$$$$$$\"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$\"\"\"\"\"\"\"\"\n");
-printf("    \"\"\"       $$$$    \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"      o$$$\n");
-printf("               \"$$$o     \"\"\"$$$$$$$$$$$$$$$$$$\"$$\"         $$$\n");
-printf("                 $$$o          \"$$\"\"$$$$$$\"\"\"\"           o$$$\n");
-printf("                  $$$$o                 oo             o$$$\"\n");
-printf("                    \"$$$$o      o$$$$$$o\"$$$$o        o$$$$\n");
-printf("                      \"$$$$$oo     \"\"$$$$o$$$$$o   o$$$$\"\"\n");
-printf("                         \"\"$$$$$oooo  \"$$$o$$$$$$$$$\"\"\"\n");
-printf("                            \"\"$$$$$$$oo $$$$$$$$$$\n");
-printf("                                     \"\"\"$$$$$$$$$$$\n");
-printf("                                        $$$$$$$$$$$$\n");
-printf("                                         $$$$$$$$$$\n");
-printf("                                          \"$$$\"\"\"\"\n");
+        printf("                        oooo$$$$$$$$$$$$oooo\n");
+        printf("                     oo$$$$$$$$$$$$$$$$$$$$$$$$o\n");
+        printf("                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$\n");
+        printf("   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$\n");
+        printf("oo $ $ \"$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$\n");
+        printf(" $$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$\n");
+        printf("  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$\n");
+        printf("  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  \"\"\"$$$\n");
+        printf("    $$$\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$\n");
+        printf("    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$o\n");
+        printf("   o$$\"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o\n");
+        printf("    $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\" \"$$$$$$ooooo$$$$o\n");
+        printf("    o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$\n");
+        printf("    $$$$$$$$\"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$\"\"\"\"\"\"\"\"\n");
+        printf("    \"\"\"       $$$$    \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"      o$$$\n");
+        printf("               \"$$$o     \"\"\"$$$$$$$$$$$$$$$$$$\"$$\"         $$$\n");
+        printf("                 $$$o          \"$$\"\"$$$$$$\"\"\"\"           o$$$\n");
+        printf("                  $$$$o                 oo             o$$$\"\n");
+        printf("                    \"$$$$o      o$$$$$$o\"$$$$o        o$$$$\n");
+        printf("                      \"$$$$$oo     \"\"$$$$o$$$$$o   o$$$$\"\"\n");
+        printf("                         \"\"$$$$$oooo  \"$$$o$$$$$$$$$\"\"\"\n");
+        printf("                            \"\"$$$$$$$oo $$$$$$$$$$\n");
+        printf("                                     \"\"\"$$$$$$$$$$$\n");
+        printf("                                        $$$$$$$$$$$$\n");
+        printf("                                         $$$$$$$$$$\n");
+        printf("                                          \"$$$\"\"\"\"\n");
 
-
-    return 0;
-}
+        return 0;
+    }
 }
